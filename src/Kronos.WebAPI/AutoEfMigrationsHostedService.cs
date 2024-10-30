0d9d5470cd1ba5169adad926d304e6efbe0aa35b
Martin Obert <30500239+martin-obert@users.noreply.github.com>
2024-10-30 11:36:01 +0100

    wip

---
+++ src/Kronos.WebAPI/AutoEfMigrationsHostedService.cs
@@ -0,0 +1,26 @@
+﻿using Kronos.WebAPI.Athena.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kronos.WebAPI;
+
+public class AutoEfMigrationsHostedService(IServiceProvider serviceProvider) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        
+// Apply migrations automatically
+        using var scope = serviceProvider.CreateScope();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<AthenaDbContext>();
+            await context.Database.MigrateAsync(stoppingToken);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while migrating the database.");
+            throw;
+        }
+    }
+}
